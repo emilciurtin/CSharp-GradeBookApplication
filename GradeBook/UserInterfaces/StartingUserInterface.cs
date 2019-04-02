a4cb6c90c7f0d5f49e32f35b5df9b9b392485a6f
Emil-Constantin Ciurtin <emil-constantin.ciurtin@michelin.com>
2019-04-02 17:12:43 +0300

    Completed the module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,14 +40,15 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
+            var type = parts[2].ToLower();
             BaseGradeBook gradeBook;
-            if (parts[2].Contains("standard"))
+            if (type == "standard")
                 gradeBook = new StandardGradeBook(name);
-            else if (parts[2].Contains("ranked"))
+            else if (type == "ranked")
                 gradeBook = new RankedGradeBook(name);
             else
             {
-                Console.WriteLine(name + " is not a supported type of gradebook, please try again");
+                Console.WriteLine(name + " is not a supported type of gradebook, please try again.");
                 return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
